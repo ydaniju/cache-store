01966e85ec7a5b397742bb66a7d016fe28729571
Yusuf Daniju <danijuyusuf@outlook.com>
2018-10-06 07:19:16 +0100

    linting

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es6": true,
+    "mocha": true
+  },
+  "extends": ["eslint:recommended", "google"],
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+};
