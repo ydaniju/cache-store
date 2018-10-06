3fce224c7af8ab5e467842f880db127f11fc1420
Yusuf Daniju <danijuyusuf@outlook.com>
2018-10-06 07:39:02 +0100

    add missing spec

--- spec/models/cache.model.spec.js
+++ spec/models/cache.model.spec.js
@@ -3,7 +3,7 @@ const Cache = require('../../app/models/cache.model');
 
 describe('Cache', () => {
   it('should be invalid if key is empty', (done) => {
-    const cache = new Cache({data: 'random'});
+    const cache = new Cache({key: null, data: 'random', expires: 123});
 
     cache.validate((err) => {
       expect(err.errors.key).to.exist;
@@ -12,10 +12,19 @@ describe('Cache', () => {
   });
 
   it('should be invalid if data is empty', (done) => {
-    const cache = new Cache({key: 'lost'});
+    const cache = new Cache({key: 'lost', data: null, expires: 123});
 
     cache.validate((err) => {
       expect(err.errors.data).to.exist;
+      done();
+    });
+  });
+
+  it('should be invalid if expires is empty', (done) => {
+    const cache = new Cache({key: 'lost', data: null, expires: null});
+
+    cache.validate((err) => {
+      expect(err.errors.expires).to.exist;
       done();
     });
   });
