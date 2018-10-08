b1531db0f99dbbcd7f8c3d1b34dc44febd96cd9a
Yusuf Daniju <danijuyusuf@outlook.com>
2018-10-08 13:53:31 +0100

    make return value cleaner

--- app/controllers/cache.controller.js
+++ app/controllers/cache.controller.js
@@ -3,7 +3,7 @@ const cacheHelper = require('../helpers/cache.helper');
 
 const CacheController = {
   index: (req, res) => {
-    return Cache.find({}, (err, caches) => {
+    return Cache.find({}, 'key', (err, caches) => {
       if (err) return res.status(500).json(err.message);
       return res.status(200).json(caches);
     });
