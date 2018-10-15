38a2c3a0b9e7668232be0ab19b7de9c118dae3a4
Yusuf Daniju <danijuyusuf@outlook.com>
2018-10-15 17:56:25 +0100

    enable cors

--- app/index.js
+++ app/index.js
@@ -5,6 +5,15 @@ const morgan = require('morgan');
 const router = require('../config/router');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 // log to console in dev
 app.use(morgan('dev'));
 // parse application/x-www-form-urlencoded
